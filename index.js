10aab2f07fe93e6d7c7286e009ba3c8da9ed9f4b
Elvis <s49188705@gmail.com>
2022-08-23 09:02:21 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    const p = document.querySelector('p');
+    p.textContent = 'This is really cool!';
+    console.log(p)
+
+})
+
+
